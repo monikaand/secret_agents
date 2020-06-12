7c405fb873e6e5eb5b911cd13a76c82f7c98ef35
m_graziowski <30260979+grazik@users.noreply.github.com>
2020-06-12 22:15:11 +0200

    get-samples-from-array | create method to get array of unique numbers in given range, add unit tests to that method

---
+++ src/helpers/getRandomNumbers.js
@@ -0,0 +1,31 @@
+function getRandomInt(max = 0, min = 0) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+export function getRandomNumbers({
+  max = 0,
+  min = 0,
+  amount = 0,
+  excluded = [],
+} = {}) {
+  if (max < min) {
+    throw Error('max has to be greater than min');
+  }
+
+  if (max - min - excluded.length + 1 < amount) {
+    throw Error('can\'t suffice amount condition');
+  }
+
+  const results = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    let number = 0;
+    let shouldRandomiseNumber = true;
+    while (shouldRandomiseNumber) {
+      number = getRandomInt(max, min);
+      shouldRandomiseNumber = results.includes(number) || excluded.includes(number);
+    }
+    results.push(number);
+  }
+  return results;
+}
